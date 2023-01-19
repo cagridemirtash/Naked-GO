e02f07a9f22517b43fbae621b49561d5f167cdbc
cagridemirtash <cagridemirtash@gmail.com>
2023-01-19 14:22:34 +0300

    Setup variable
    Difference between implicit type and normal declaration

--- go-starter/main.go
+++ go-starter/main.go
@@ -2,10 +2,18 @@ package main
 
 import "fmt"
 
+var c, python, java bool
+var l,j,k int = 1,2,3
 func main() {
 	fmt.Println("Hello, world!")
+	//The implicit type of variable (:=) -> var [variable name] type 
 	a, b:= show();
 	fmt.Println(a + b)
+
+	//The Using Bool Type
+	var i int
+	fmt.Println(i, c, python, java)
+	fmt.Println("l j k :",l, j, k)
 }
 
 func show() (x,y int) {
