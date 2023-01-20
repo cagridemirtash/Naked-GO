385da36813bdb25ae9f90f89b3b1c43fb37b79f9
cagridemirtash <cagridemirtash@gmail.com>
2023-01-20 13:38:19 +0300

    Default value of Types
    When the decleration of variable without value, default value assign to which type of variable(string, int etc)

--- basic-types/main.go
+++ basic-types/main.go
@@ -15,13 +15,23 @@ import (
 * complex64, complex128
 */
 
+/*
+* Variables when the initial value is not assigned, the default value is assigned.
+* For Example: int -> 0, float -> 0.0, string -> "", bool -> false
+ */
+
 var name string = "Çağrı Demirtaş"
 var age, height int64 = 23, 189
 var isBadSide bool = true
+var cagri string
+var first, second, third int
 
 func main() {
 	fmt.Println("Your Name :", name)
 	fmt.Println("Your Age :", age, "Your Height :", height)
+	// The initialize values which are cagri => "", first, second, third are 0
+	// Assign to default value from GO.
+	fmt.Println(cagri, first, second, third)
 	// !If Condition -> We can use with assign a variable and use it
 	if condition := isBadSide; condition {
 		fmt.Println("You are a bad side")
