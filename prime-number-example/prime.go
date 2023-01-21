4066103e5fd6f785f231f518f044a9604b97f17a
Çağrı Demirtaş <cagridemirtash@gmail.com>
2023-01-21 17:27:53 +0300

    Add the other form of for and while in go

--- prime-number-example/prime.go
+++ prime-number-example/prime.go
@@ -7,6 +7,14 @@ import (
 func main() {
 	var isPrime bool = true
 	var primeNumber int = 21
+
+	/* Other Form of using for
+		for ; sum < 1000; {
+		sum += sum
+		fmt.Println(sum)
+	}
+	*/
+
 	for i := 2; i < primeNumber; i++ {
 		if primeNumber%i == 0 {
 			isPrime = false
@@ -17,4 +25,13 @@ func main() {
 	} else {
 		fmt.Println("Not prime number")
 	}
+
+	// So how to use while loop in go
+
+	/* Drop the semi colons and write for instead of while
+	sum := 1
+	for sum < 1000 {
+		sum += sum
+	}
+	*/
 }
