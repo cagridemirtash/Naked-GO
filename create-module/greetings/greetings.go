5aed8506b7f2892e775c6e0e476700e8c71a9c83
cagridemirtash <cagridemirtash@gmail.com>
2023-02-01 14:04:12 +0300

    Add error handling with errors package

--- create-module/greetings/greetings.go
+++ create-module/greetings/greetings.go
@@ -1,8 +1,16 @@
 package greetings
 
-import "fmt"
+import (
+	e "errors"
+	f "fmt"
+)
 
-func Hello(name string) string {
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+func Hello(name string) (string, error) {
+	//When name is blank string, return an error which is empty name
+	if name == "" {
+		return "", e.New("empty name")
+	}
+	message := f.Sprintf("Hi, %v. Welcome!", name)
+	//When the assign is ok, return message and nil(for handle errors).
+	return message, nil
 }
--- create-module/main.go
+++ create-module/main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
-	"example.com/greetings"
-	"fmt"
+	g "example.com/greetings"
+	f "fmt"
+	l "log"
 )
 
 func main() {
-	// Get a greeting message and print it.
-	message := greetings.Hello("Çağrı")
-	fmt.Println(message)
+	//Log package hold some methods
+	//SetPrefix is prefix which is throw an error when assign to message
+	l.SetPrefix("greetings : ")
+
+	l.SetFlags(0)
+	// Get a greeting message and handle error mechanism
+	message, err := g.Hello("")
+	//If error was found, this if block runs and
+	if err != nil {
+		l.Fatal(err)
+	}
+	//If return not a error, print the message on console.
+	f.Println(message)
 }
